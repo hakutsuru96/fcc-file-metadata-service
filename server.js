03a990522c6142924095d5224e9f52697780ed8a
Glitch (legend-broccoli) <none>
2017-12-09 06:42:00 +0000

    📟👄 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var multer = require('multer')
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/file", multer().single('file'), function(req, res) {
+  res.json({
+    size: req.file.size
+  })
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
